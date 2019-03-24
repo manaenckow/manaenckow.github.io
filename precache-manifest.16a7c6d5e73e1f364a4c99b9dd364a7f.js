04013a5890e14703586b048ad133b6e805832acc
unknown <manaenkov.2000@gmail.com>
2019-03-24 15:05:22 +0300

    Updates

---
+++ precache-manifest.16a7c6d5e73e1f364a4c99b9dd364a7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "b517672f704f1ac8a3b7",
+    "url": "/static/js/main.b517672f.chunk.js"
+  },
+  {
+    "revision": "baca993ac1348da69a74",
+    "url": "/static/js/2.baca993a.chunk.js"
+  },
+  {
+    "revision": "b517672f704f1ac8a3b7",
+    "url": "/static/css/main.0c3186e4.chunk.css"
+  },
+  {
+    "revision": "baca993ac1348da69a74",
+    "url": "/static/css/2.62e8debc.chunk.css"
+  },
+  {
+    "revision": "94448a9402923a0cb674c6ac5f625fad",
+    "url": "/index.html"
+  }
+];
